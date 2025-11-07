99c6ca17fee6068ec6d44f6a1ab2606cbe8a5614
hardingyan <1173529071@qq.com>
2025-11-07 18:21:27 +0800

    add diff_tensors.py

--- Attention/FlashAttentionV2.py
+++ Attention/FlashAttentionV2.py
@@ -1,8 +1,82 @@
 import math
 import torch
 
-from Ref import get_kv, repeat_kv
+from Ref import repeat_kv
+from diff_tensors import compute_diff, print_diff
 
+def gqa_trick(seq_len, num_heads, qk_head_size, kv_len, num_kv_heads, v_head_size):
+    def ref(q, k ,v):
+        q = q.clone()
+        k = k.clone()
+        v = v.clone()
+
+        k, v = repeat_kv(k, v, 1, num_heads // num_kv_heads)
+
+        q = q.transpose(0, 1).view(num_heads, seq_len, qk_head_size)
+        k = k.transpose(0, 1).view(num_heads, kv_len, qk_head_size)
+        v = v.transpose(0, 1).view(num_heads, kv_len, v_head_size)
+
+        kT = k.transpose(1, 2).view(num_heads, qk_head_size, kv_len)
+
+        qkT = torch.bmm(q, kT)  # (num_heads,seq_len, kv_len)
+
+        o = torch.matmul(qkT, v)
+        o = o.transpose(0, 1).view(seq_len, num_heads, v_head_size)
+
+        return o
+
+    def act(q, k, v):
+        q = q.clone()
+        k = k.clone()
+        v = v.clone()
+
+        group_num = num_kv_heads
+        group_size = num_heads // group_num
+
+        q = q.view(seq_len, group_num, group_size, qk_head_size)
+        k = k.view(kv_len, group_num, 1, qk_head_size)
+        v = v.view(kv_len, group_num, 1, v_head_size)
+
+        out = torch.zeros(
+            (seq_len, group_num, group_size, qk_head_size), dtype=dtype, device=device
+        )
+
+        for g0 in range(group_num):
+            group_slice = slice(g0, g0 + 1)
+            q_slice = q[:, group_slice, :, :].reshape(seq_len * group_size, qk_head_size)
+            k_slice = k[:, group_slice, :, :].reshape(kv_len, qk_head_size)
+            v_slice = v[:, group_slice, :, :].reshape(kv_len, v_head_size)
+
+            kT = k_slice.transpose(0, 1).view(qk_head_size, kv_len)
+
+            qkT = torch.matmul(q_slice, kT)  # (seq_len*group_size, kv_len)
+
+            o = torch.matmul(qkT, v_slice)  # (seq_len*group_size, v_head_size)
+            o = o.view(seq_len, 1, group_size, v_head_size)
+
+            out[:, group_slice, :, :] = o
+
+        out = out.view(seq_len, num_heads, qk_head_size)
+
+        return out
+
+    dtype = torch.float16
+    device = "cpu"
+
+    q = torch.randn(seq_len, num_heads, qk_head_size, dtype=dtype, device=device)
+    k = torch.randn(kv_len, num_kv_heads, qk_head_size, dtype=dtype, device=device)
+    v = torch.randn(kv_len, num_kv_heads, v_head_size, dtype=dtype, device=device)
+
+    out_act = act(q, k, v)
+    out_ref = ref(q, k, v)
+
+    out_act = out_act.detach().numpy()
+    out_ref = out_ref.detach().numpy()
+
+    abs_error, rel_error, top_indices = compute_diff(out_act, out_ref, idx_cnt=20)
+    print_diff(
+        out_act, out_ref, "act", "ref", abs_error, rel_error, top_indices, eps=1e-1
+    )
 
 def paged_flash_attention_v2(q, k_cache, v_cache, page_table, seq_lengths_host, kv_lengths_host, mask, qk_scale=None):
     """
@@ -19,3 +93,8 @@ def paged_flash_attention_v2(q, k_cache, v_cache, page_table, seq_lengths_host, 
     offset = 0
 
     return outs
+
+
+if __name__ == "__main__":
+    # seq_len, num_heads, qk_head_size, kv_len, num_kv_heads, v_head_size
+    gqa_trick(10, 16, 128, 20, 2, 128)
--- Attention/test.py
+++ Attention/test.py
@@ -1,59 +1,10 @@
 import numpy as np
 
 import torch
-from torch import nn
-import torch.nn.functional as F
+from diff_tensors import compute_diff, print_diff
 
-from Ref import paged_attention
-from Ref import attention
-from Ref import flash_attention_v2
-
+from Ref import paged_attention, attention, flash_attention_v2
 from FlashAttentionV2 import paged_flash_attention_v2 as paged_attention_act
-
-
-
-def compute_diff(array1, ref_array2, idx_cnt=5):
-    assert (
-        array1.shape == ref_array2.shape
-    ), f"Shape mismatch between arrays, {array1.shape} and {ref_array2.shape}"
-
-    abs_diff = np.abs(array1 - ref_array2)
-
-    max_abs_diff = abs_diff.max()
-    range_ref = np.abs(ref_array2).max()
-
-    max_rel_diff = max_abs_diff / range_ref.max()
-
-    idx_cnt = min(idx_cnt, abs_diff.size)
-
-    flat_indices = np.argsort(abs_diff.ravel())[-idx_cnt:][::-1]
-
-    top_indices = np.array(np.unravel_index(flat_indices, abs_diff.shape)).T
-
-    return max_abs_diff, max_rel_diff, top_indices
-
-
-def print_diff(arr_act, arr_ref, tag_act, tag_ref, abs_diff, rel_diff, top_indices, eps):
-    if rel_diff < eps:
-        print(f"{tag_act} vs {tag_ref} Pass!")
-        return
-
-    print("-" * 65)
-    print(f"{tag_act} vs {tag_ref}, abs err {abs_diff}, ref err {rel_diff}")
-
-    print(f"{'Index':<20} {tag_act:<15} {tag_ref:<15} {'Abs Diff':<15}")
-
-    for idx in top_indices:
-        act_val = arr_act[tuple(idx)]
-        ref_val = arr_ref[tuple(idx)]
-        idx_diff = abs(act_val - ref_val)
-
-        idx_str = str(tuple(map(int, idx)))
-
-        if isinstance(act_val, (int, np.integer)):
-            print(f"{idx_str:<20} {act_val:<15} {ref_val:<15} {idx_diff:<15}")
-        else:
-            print(f"{idx_str:<20} {act_val:<15.6f} {ref_val:<15.6f} {idx_diff:<15.6f}")
 
 
 def generate_seq_lengths(seq_len, num_batches):
